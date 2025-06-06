ae3166123361833efa075748e788b70390f55501
Abdulrehmankhan9 <abdulrehmankhan.ak91@gmail.com>
2025-06-06 23:08:22 +0500

    update view, added new templates

--- todo_pro/todo_app/views.py
+++ todo_pro/todo_app/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 def home(request):
     return render(request, 'home.html')
+
+def register_view(request):
+    return render(request, 'register.html')
+
+def login_view(request):
+    return render(request, 'login.html')
+
+def logout_view(request):
+    return HttpResponse("Logout")
+
+def delete_task(request, id):
+    return HttpResponse("delete")
