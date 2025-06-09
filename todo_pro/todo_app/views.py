3a619d7a6d22ef637afc8778b648230e0dd79e1d
Abdulrehmankhan9 <abdulrehmankhan.ak91@gmail.com>
2025-06-09 15:37:41 +0500

    update logout

--- todo_pro/todo_app/views.py
+++ todo_pro/todo_app/views.py
@@ -37,7 +37,7 @@ def login_view(request):
 @login_required
 def logout_view(request):
     logout(request)
-    return HttpResponse("Logout")
+    return redirect('login')
 
 def delete_task(request, id):
     return HttpResponse("delete")
